6fe2566eca1f96f7701941f0649fc274484902e6
SujoyMondol <76837661+SujoyMondol@users.noreply.github.com>
2024-05-03 23:10:38 -0400

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "SK E-Commerce",
+  description: "The best shopping experience you can have",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="p-4 max-w-7xl m-auto min-w-[300px]">{children}</main>
+      </body>
+    </html>
+  );
+}
